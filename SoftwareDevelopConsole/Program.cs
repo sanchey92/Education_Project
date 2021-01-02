edcfe19e0180cd7cc3e004aa647e30775e971d3a
alexandr <sashkaageev@gmail.com>
2021-01-02 16:44:26 +0300

    Initial commit

---
+++ SoftwareDevelopConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareDevelopConsole
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
