1ddc4c0d8b56e6c0d0b058e6eca3f265ca57fe44
alexandr <sashkaageev@gmail.com>
2021-01-02 19:54:01 +0300

    Added new Projects

---
+++ SoftwareDevelopDomain/Person.cs
@@ -0,0 +1,7 @@
+namespace SoftwareDevelopDomain
+{
+    public class Person
+    {
+        
+    }
+}
---
+++ SoftwareDevelopPersistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SoftwareDevelopPersistence
+{
+    public class Class1
+    {
+    }
+}
